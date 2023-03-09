a234473f89a615d291ee39eabb71b31c095e7403
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2023-03-09 19:45:44 +0000

    updated

--- exercism/space-age/src/lib.rs
+++ exercism/space-age/src/lib.rs
@@ -2,12 +2,12 @@ const EARTH_YEARS_SECONDS: u64 = 31557600;
 
 const MERCURY: f64 = 0.2408467;
 const VENUS: f64 = 0.61519726;
-const Earth: f64 = 1.0;
-const Mars: f64 = 1.8808158;
-const Jupiter: f64 = 11.862615;
-const Saturn: f64 = 29.447498;
-const Uranus: f64 = 84.016846;
-const Neptune: f64 = 164.79132;
+const EARTH: f64 = 1.0;
+const MARS: f64 = 1.8808158;
+const JUPITER: f64 = 11.862615;
+const SATURN: f64 = 29.447498;
+const URANUS: f64 = 84.016846;
+const NEPTUNE: f64 = 164.79132;
 
 #[derive(Debug)]
 pub struct Duration(u64);
@@ -33,11 +33,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / MERCURY as f64
+    }
+}
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / VENUS as f64
+    }
+}
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        d.0 as f64 * EARTH / EARTH_YEARS_SECONDS as f64
+    }
+}
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / MARS as f64
+    }
+}
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / JUPITER as f64
+    }
+}
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / SATURN as f64
+    }
+}
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / URANUS as f64
+    }
+}
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        (d.0 as f64 / EARTH_YEARS_SECONDS as f64) / NEPTUNE as f64
+    }
+}
